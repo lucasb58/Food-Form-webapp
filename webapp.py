717f80ff71f0e7c90301c8b73391c215c97b669b
unknown <mrlucasblessing@gmail.com>
2024-09-27 13:56:48 -0700

    coded

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request 
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
@@ -8,7 +8,17 @@ def render_main():
     
 @app.route("/input")
 def render_main_input():
-    return render_template('input.html')
+    Q1 = request.args['Q1']
+    if Q1 == "Meal":
+        answer = "Teriyaki Chicken"
+        
+    if Q1 == "Snack":
+        answer = "Onigiri"
+        
+    if Q1 == "Dessert":
+        answer = "Mochi"    
+        
+    return render_template('input.html', response1 = answer)
 
 if __name__=="__main__":
-    app.run(debug=False)
+    app.run(debug=True)
