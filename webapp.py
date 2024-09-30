8f5c8c364577dffa631b1ed249b17b2ebba6b9a1
unknown <mrlucasblessing@gmail.com>
2024-09-30 14:20:06 -0700

    codoed

--- webapp.py
+++ webapp.py
@@ -7,18 +7,43 @@ def render_main():
     return render_template('home.html')
     
 @app.route("/input")
-def render_main_input():
+def render_main_q1():
     Q1 = request.args['Q1']
     if Q1 == "Meal":
-        answer = "Teriyaki Chicken"
+        answer1 = "Teriyaki Chicken"
         
     if Q1 == "Snack":
-        answer = "Onigiri"
+        answer1 = "Onigiri"
         
     if Q1 == "Dessert":
-        answer = "Mochi"    
+        answer1 = "Mochi"    #q1
         
-    return render_template('input.html', response1 = answer)
+    Q2 = request.args['Q2']
+    if Q2 == "Hot":
+        answer2 = "Ramen"
+        
+    if Q2 == "Cold":
+        answer2 = "Soba"    #q2
+        
+    Q3 = request.args['Q3']
+    if Q3 == "Sweet":
+        answer3 = "Dango"
+        
+    if Q3 == "Sour":
+        answer3 = "Umeboshi" 
+        
+    if Q3 == "Spicy":
+        answer3 = "Chicken Curry Katsu"
+        
+    if Q3 == "Bitter":
+        answer3 = "Natto"
+        
+    if Q3 == "Salty":
+        answer3 = "Uni Sushi" #q3 
+    
+    return render_template('input.html', response1 = answer1, response2 = answer2, response3 = answer3, )
+    
+    
 
 if __name__=="__main__":
     app.run(debug=True)
